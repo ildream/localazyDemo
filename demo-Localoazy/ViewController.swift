3d6b411b52456ae736e487469896be4447b0ad45
jay <jay.wang@coins.ph>
2025-08-12 18:07:26 +0800

    fix: first commit

---
+++ demo-Localoazy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demo-Localoazy
+//
+//  Created by Jay Wang on 2023/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
